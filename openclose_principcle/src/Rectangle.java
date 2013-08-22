44b5b8f89bafda8385fa9ed5ee97c1437ea73a52
bomnich18 <bomnich18@yahoo.com.vn>
2013-08-22 17:09:56 +0700

    remove wrong directory

--- openclose_principcle/src/Rectangle.java
+++
@@ -1,9 +0,0 @@
-public class Rectangle extends Shape {
-
-	@Override
-	void draw() {
-		// TODO Auto-generated method stub
-		System.out.println("Rectangle");
-	}
-
-}
--- openclose_principcle/src/Shape.java
+++
@@ -1,4 +0,0 @@
-
-abstract class Shape {
-	 abstract void draw();
-}
